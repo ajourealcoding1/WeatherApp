7dec545b600d72b31b96a97ce70684715bc5958b
JooJaeLin <jl1096@ajou.ac.kr>
2019-07-27 15:20:45 +0900

    13. flexbox

--- App.js
+++ App.js
@@ -1,34 +1,6 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-class CounterButton extends React.Component {
-static defaultProps={
-counter: -1,
-};
-
-constructor(props){
-super(props);
-
-this.state={
-counter:props.counter,
-};
-}
-
-clickHandler=()=>{
-this.setState({
-counter:this.state.counter+1,
-});
-};
-
-render(){
-  return (
-    <View style={styles.buttonContainer}>
-       <Text>{this.state.counter}</Text>
-        <Button title={'click me!'} onPress={this.clickHandler}/>
-    </View>
-  );
-}
-}
 
 
 export default class App extends React.Component {
@@ -36,10 +8,9 @@ export default class App extends React.Component {
   render() {
  return (
       <View style={styles.container}>
-           <CounterButton counter={0} />
-           <CounterButton count={1} />
-           <CounterButton counter={2} />
-           <CounterButton counter={3} />
+        <Text>1</Text>
+        <Text>2</Text>
+        <Text>3</Text>
       </View>
     );
   }
@@ -50,10 +21,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+     alignItems: 'center',
   },
-  buttonContainer:{
-  width:'100%',
-  },
+
 });

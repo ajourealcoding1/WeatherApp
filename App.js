c9014e05ca211c55748cd77d7a563fad5575a438
JooJaeLin <jl1096@ajou.ac.kr>
2019-07-27 15:07:24 +0900

    05. handler:function

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Button, StyleSheet,  View } from 'react-native';
 
-export default function App() {
+export default class App extends React.Component {
+clickHandler = () => console.log('Clicked!');
+
+render(){
   return (
     <View style={styles.container}>
-        <Button title={'click me!'} onPress={() => console.log('Clicked!')}/>
+        <Button title={'click me!'} onPress={this.clickHandler}/>
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({

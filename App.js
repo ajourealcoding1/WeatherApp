1178f45e1c3bf40be408f444d9c2ab0f34b9015f
JooJaeLin <jl1096@ajou.ac.kr>
2019-07-27 15:13:03 +0900

    08. Custom component: more components

--- App.js
+++ App.js
@@ -29,6 +29,9 @@ export default class App extends React.Component {
  return (
       <View style={styles.container}>
         <CounterButton />
+         <CounterButton />
+          <CounterButton />
+           <CounterButton />
       </View>
     );
   }

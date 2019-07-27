e27a9cb4b2876de85cb4362ac26397ba201997f7
JooJaeLin <jl1096@ajou.ac.kr>
2019-07-28 01:13:47 +0900

    Add start page

--- App.js
+++ App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import CityList from './CityList'
 import WeatherDetailScreen from './WeatherDetailScreen';
+import StartPage from './StartPage';
 
 
  const AppNavigator = createStackNavigator(
     {
+    StartPage : StartPage,
     CityList : CityList,
     Detail : WeatherDetailScreen,
     },
     {
-    initialRouteName: 'CityList',
+    initialRouteName: 'StartPage',
     }
     );
 
